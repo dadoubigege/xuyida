5ba548059da525d8a38ecd9b5fc96736d7e10cf4
Xuyida <1019211573@qq.com>
2018-08-22 17:31:39 +0800

    tittle

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default="作品的描述" , max_length=100)
+    image =models.ImageField(default="default.png" , upload_to = 'Image/')
+    title = models.CharField(default="作品的名称" , max_length=50)
+
+    def __str__(self):
+        return self.title
